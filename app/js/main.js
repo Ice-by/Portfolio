e2ef658fcd048adbe5d7b0bfb1975cc097d416eb
Ice <icebelarus@gmail.com>
2021-02-08 23:51:55 +0300

    fix project 8

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
+    var tabs = document.querySelector('.slider__img-container');
+document.querySelector('.slider__select-items').addEventListener('change', function() {
+  tabs.querySelector('.slider__active-tab').classList.remove('slider__active-tab');
+  tabs.querySelector(`#${this.value}`).classList.add('slider__active-tab');  
+});
 
     
 })
