d6860141e91026ca5d38a53fefea50393d084a42
Ice <icebelarus@gmail.com>
2021-02-11 03:23:14 +0300

    add tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,22 @@
 $(function(){
-    var tabs = document.querySelector('.slider__img-container');
-document.querySelector('.slider__select-items').addEventListener('change', function() {
-  tabs.querySelector('.slider__active-tab').classList.remove('slider__active-tab');
-  tabs.querySelector(`#${this.value}`).classList.add('slider__active-tab');  
-});
 
-    
+  let tabs = document.querySelector('.container')
+  let active = tabs.querySelectorAll('.menu__item')
+  let items = tabs.querySelectorAll('.tab-item')
+  let items2 = tabs.querySelectorAll('.tab-item2')
+
+  function change(arr, i) {
+    arr.forEach( item => {
+      item.forEach( i => {i.classList.remove('tab-active')})
+      item[i].classList.add('tab-active')
+    })
+  }
+
+  for(let i = 0; i < active.length; i++) {
+    active[i].addEventListener('click', () => {
+      change([active, items, items2], i)
+    })
+  }
+
+
 })
